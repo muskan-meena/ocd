d60332ce0d876faa00bc136a9245cec78411383d
muskan-meena <akshatmeena987@gmail.com>
2025-03-19 23:34:57 +0530

    Update day17.cpp

--- day17.cpp
+++ day17.cpp
@@ -1,5 +1,62 @@
-//ques
-//app
-//tc
-//sc
-class solution
+ques 1 (isomorphic string)
+TC=O(n)
+SC=O(n)
+        
+class solution{
+public:
+    bool isIsomorphic(string s, string t) {
+      if (s.length() != t.length())
+        return false;
+      unordered_map<char, char>mapST;
+      unordered_map<char, char>mapTS;
+     for( i=0; i<s.length(); i++)
+{    
+        if(mapST.find(s[i]) != mapST.end()){
+            if(mapST[s[i]] != t[i])
+{        
+                  return false;
+            }
+  }
+        else {
+           mapST[s[i]] =t[i];
+        }
+      if(mapTS.find(t[i]) != mapTS.end()){
+         if(mapTS[t[i]] != s[i])
+{     
+        return false;
+}
+      }
+        else {
+          mapTS[t[i]] = s[i];
+        }
+}
+   return true;
+    }
+
+
+ques 2 ( remove outermost parentheses)
+TC=O(n)
+SC=O(n)
+
+class solution (
+public:
+    string removeOuterParentheses(string s){
+       int count = 0;
+       string result = "";
+
+      for (char c : s){
+           if (c=='('){
+               if (count>0){
+                   result += c;
+               }
+                count++;
+           }
+             eise if (c == ')'){
+                count--;
+                if (count > 0) {
+                result += c;
+                }
+             }
+      }
+        return result;
+}
