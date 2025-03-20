0095f3c56b1df5069af8b34507bfcb2eef01e74c
muskan-meena <akshatmeena987@gmail.com>
2025-03-20 22:30:14 +0530

    Update day18.cpp

--- day18.cpp
+++ day18.cpp
@@ -1,1 +1,37 @@
+TC=O(NLOGN)
+SC=O(1)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
+
+        return (s==t);
+
+    }
+};
+//ques->2longest common pefix
+// TC : O(n^2) 
+//SC : O(1)
+
+class Solution {
+    public:
+        string longestCommonPrefix(vector<string>& strs) {
+            if (strs.empty()) return "";
+
+            string prefix = strs[0]; 
+
+            for (int i = 1; i < strs.size(); i++) {
+                while (strs[i].find(prefix) != 0) { 
+                    prefix.pop_back();
+                    if (prefix.empty()) return "";
+                }
+            }
+
+            return prefix;
+        }
+    };
