0a10af9499e0ed3bc9dbfecf0ed23e36a3712863
muskan-meena <akshatmeena987@gmail.com>
2025-03-20 22:38:14 +0530

    Update day18.cpp

--- day18.cpp
+++ day18.cpp
@@ -4,14 +4,13 @@ SC=O(1)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-          return s==t;
+         if(s==t){
+             return true;
 
     }
+        return false;
 };
 ques=2(longest common pefix)
 TC : O(n^2) 
