8f92851c1702cddbc9726df68ca35eb5a7a131c6
muskan-meena <akshatmeena987@gmail.com>
2025-03-28 03:19:34 +0530

    Update day25.cpp

--- day25.cpp
+++ day25.cpp
@@ -1,1 +1,14 @@
+ques 1 (deleted node in a linked list )
+TC=O(1)
+SC=O(1)
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val; 
+        ListNode* temp = node->next; 
+        node->next = node->next->next;
+        delete temp;  
+    }
+};
 
+ques 2 (
