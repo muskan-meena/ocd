6b2de461ea4c3fa55db14d095af285de031579a4
muskan-meena <akshatmeena987@gmail.com>
2025-03-28 03:23:04 +0530

    Update day25.cpp

--- day25.cpp
+++ day25.cpp
@@ -12,3 +12,39 @@ public:
 };
 
 ques 2 (
+
+
+
+// Definition for singly-linked list node
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int val) { 
+        data = val; 
+        next = nullptr; 
+    }
+};
+
+// Function to insert a node at the end of the linked list
+Node* insertAtEnd(Node* head, int x) {
+    Node* newNode = new Node(x);
+    
+    // If the linked list is empty
+    if (head == nullptr) {
+        return newNode;
+    }
+
+    Node* temp = head;
+    
+    // Traverse to the last node
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    
+    // Insert the new node at the end
+    temp->next = newNode;
+
+    return head;
+}
+
